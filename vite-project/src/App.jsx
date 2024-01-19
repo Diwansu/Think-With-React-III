99ab54c74708853ee70cc9c840390f73ee4c0c55
Diwanshu <diwansu531@gmail.com>
2024-01-19 15:04:47 +0530

    done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        {/* <a href="https://vitejs.dev" target="_blank"> */}
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        {/* </a> */}
+        {/* <a href="https://react.dev"÷ target="_blank"> */}
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* </a> */}
       </div>
       <h1>Hello Kalvium </h1>
      
